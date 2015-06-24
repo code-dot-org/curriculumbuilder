4b3f5a6d1717d1b6c59bc1a06ca084dfd00a02b1
Josh Caldwell <josh@code.org>
2015-06-24 11:49:11 -0700

    More WSGI fun

---
+++ lessons/migrations/0003_auto_20150624_1139.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('lessons', '0002_auto_20150623_2105'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='lesson',
+            name='anchor_standards',
+            field=models.ManyToManyField(related_name='anchors', to='standards.Standard', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='lesson',
+            name='standards',
+            field=models.ManyToManyField(to='standards.Standard', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='lesson',
+            name='vocab',
+            field=models.ManyToManyField(to='lessons.Vocab', blank=True),
+        ),
+    ]
