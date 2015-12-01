dfb0fd24daa7414c64d38a4f3b40be9a588b3c01
Josh Caldwell <josh@code.org>
2015-12-01 10:33:57 -0800

    Fix individual standard view

--- standards/views.py
+++ standards/views.py
@@ -17,7 +17,7 @@ def index(request):
   curricula = Curriculum.objects.all()
   return render(request, 'standards/index.html', {'frameworks': frameworks, 'curricula': curricula})
 
-def by_framework(request, slug):
+def by_framework(request, slug, curriculum_slug = None):
   framework = get_object_or_404(Framework, slug = slug)
   top_categories = Category.objects.filter(framework=framework, parent__isnull=True)\
     .prefetch_related('children__standard_set__lesson_set', 'standard_set__lesson_set')
@@ -30,12 +30,14 @@ def by_curriculum(request, slug):
   return render(request, 'standards/curriculum.html', {'curriculum': curriculum})
 
 def single_standard(request, slug, shortcode):
-  standard = get_object_or_404(Standard.objects.prefetch_related('lesson_set__unitlesson_set__unit__curriculum'), framework__slug=slug, shortcode=shortcode)
+  #standard = get_object_or_404(Standard.objects.prefetch_related('lesson_set__unitlesson_set__unit__curriculum'), framework__slug=slug, shortcode=shortcode)
+  standard = get_object_or_404(Standard, framework__slug=slug, shortcode=shortcode)
   return render(request, 'standards/standard.html', {'standard': standard})
 
 @api_view(['GET',])
 def standard_element(request, slug, shortcode, format=None):
-  standard = get_object_or_404(Standard.objects.prefetch_related('lesson_set__unitlesson_set__unit__curriculum'), framework__slug=slug, shortcode=shortcode)
+  #standard = get_object_or_404(Standard.objects.prefetch_related('lesson_set__unitlesson_set__unit__curriculum'), framework__slug=slug, shortcode=shortcode)
+  standard = get_object_or_404(Standard, framework__slug=slug, shortcode=shortcode)
 
   serializer = StandardSerializer(standard)
   return Response(serializer.data)
