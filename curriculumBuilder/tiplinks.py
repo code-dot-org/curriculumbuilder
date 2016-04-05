ef79ec3d00f6f6471d7a6b6d3f428ea38f863cd4
Josh Caldwell <josh@code.org>
2016-04-04 23:07:41 -0700

    Tweak to margin icons

--- curriculumBuilder/tiplinks.py
+++ curriculumBuilder/tiplinks.py
@@ -1,13 +1,12 @@
 from markdown.util import etree
 from markdown.extensions import Extension
-from markdown.preprocessors import Preprocessor
 from markdown.inlinepatterns import Pattern
 
 TIPLINK_RE = r'(?:^|\n)?([\w\-]+)!!!\ ?([\w\-]+)?'
 
 class TipLinksExtensions(Extension):
   def extendMarkdown(self, md, md_globals):
-    tiplink_tag = TipTagPattern(TIPLINK_RE, 'a', {})
+    tiplink_tag = TipTagPattern(TIPLINK_RE, 'div', {})
     md.inlinePatterns.add('tiplink', tiplink_tag, '_begin')
 
 class TipTagPattern(Pattern):
@@ -25,16 +24,15 @@ class TipTagPattern(Pattern):
     tip_type = m.group(2)
     tip_link = m.group(3)
     el = etree.Element(self.tag)
-    el.set('href', '#%s_%s' % (tip_type, tip_link))
     el.set('class', 'tiplink tiplink-%s' % (tip_type))
     if tip_type == 'tip':
-      el.text = '<i class="glyphicon glyphicon-alert"></i>'
+      el.text = '<a href="#%s_%s"><i class="glyphicon glyphicon-alert"></i></a>' % (tip_type, tip_link)
     elif tip_type == 'discussion':
-      el.text = '<i class="glyphicon glyphicon-comment"></i>'
+      el.text = '<a href="#%s_%s"><i class="glyphicon glyphicon-comment"></i></a>' % (tip_type, tip_link)
     elif tip_type == 'content':
-      el.text = '<i class="glyphicon glyphicon-education"></i>'
+      el.text = '<a href="#%s_%s"><i class="glyphicon glyphicon-education"></i></a>' % (tip_type, tip_link)
     else:
-      el.text = '<i class="glyphicon glyphicon-alert"></i>'
+      el.text = '<a href="#%s_%s"><i class="glyphicon glyphicon-alert"></i></a>' % (tip_type, tip_link)
 
     for (key,val) in self.attrs.items():
       el.set(key,val)
