8019942f41894998163a201a269ba937d2058cba
Josh Caldwell <josh@code.org>
2016-08-17 14:36:46 -0700

    Allow period in documentation slug for console.log

--- documentation/urls.py
+++ documentation/urls.py
@@ -5,5 +5,5 @@ from documentation import views
 urlpatterns = patterns('curricula.views',
                         #url(r'^$', views.index, name='home'),
                         url(r'^(?P<slug>[-\w]+)/$', views.ide_view, name='ide_view'),
-                        url(r'^(?P<ide_slug>[-\w]+)/(?P<slug>[-\w]+)/$', views.block_view, name='block_view'),
+                        url(r'^(?P<ide_slug>[-\w]+)/(?P<slug>[-\w.]+)/$', views.block_view, name='block_view'),
                        )
