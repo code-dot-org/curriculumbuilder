12540d0484bd22b92391d9b0fcb7d551d6dc79ee
Josh Caldwell <josh@code.org>
2016-11-16 11:50:49 -0800

    Enable local caching of compressed CSS

--- curriculumBuilder/s3utils.py
+++ curriculumBuilder/s3utils.py
@@ -1,5 +1,26 @@
+from django.core.files.storage import get_storage_class
 from storages.backends.s3boto import S3BotoStorage
 
 StaticRootS3BotoStorage = lambda: S3BotoStorage(location='static')
 MediaRootS3BotoStorage  = lambda: S3BotoStorage(location='media')
 CurriculumRootS3BotoStorage  = lambda: S3BotoStorage(location='curriculum')
+
+
+class CachedS3BotoStorage(StaticRootS3BotoStorage):
+    """
+    S3 storage backend that saves the files locally, too.
+    """
+    def __init__(self, *args, **kwargs):
+        super(CachedS3BotoStorage, self).__init__(*args, **kwargs)
+        self.local_storage = get_storage_class(
+            "compressor.storage.CompressorFileStorage")()
+
+    def save(self, name, content):
+        self.local_storage._save(name, content)
+        super(CachedS3BotoStorage, self).save(name, self.local_storage._open(name))
+        return name
+
+    def get_available_name(self, name):
+        """ Always overwrite existing file with the same name. """
+        name = self._clean_name(name)
+        return name
--- curriculumBuilder/settings.py
+++ curriculumBuilder/settings.py
@@ -578,8 +578,9 @@ CELERY_TIMEZONE = 'America/Los_Angeles'
 # COMPRESS SETTINGS #
 #####################
 
-COMPRESS_STORAGE = 'curriculumBuilder.s3utils.StaticRootS3BotoStorage'
+COMPRESS_STORAGE = 'curriculumBuilder.s3utils.CachedS3BotoStorage'
 COMPRESS_URL = STATIC_URL
+COMPRESS_ROOT = STATIC_ROOT
 
 ###########
 # LOGGING #
