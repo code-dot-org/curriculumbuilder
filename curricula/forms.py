b687789c72e20c5c27311cfb0b7c4a1f37006d95
Josh Caldwell <josh@code.org>
2016-11-28 10:30:54 -0800

    Make changelog comment a textfield

--- curricula/forms.py
+++ curricula/forms.py
@@ -1,5 +1,11 @@
 from django import forms
+# from mezzanine.conf import settings
+# from mezzanine.utils.importing import import_dotted_path
 
 
 class ChangelogForm(forms.Form):
-    comment = forms.CharField(label='Comment', max_length=255)
+
+    # In case we want to support md later...
+    # richtext_widget = import_dotted_path(settings.RICHTEXT_WIDGET_CLASS)
+
+    comment = forms.CharField(label='Comment', widget=forms.Textarea(attrs={'rows': 5, 'cols': 35}))
