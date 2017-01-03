f5d16a03df886ae995096dca8b1c748a2a58b8cf
Josh Caldwell <josh@code.org>
2017-01-03 13:20:06 -0800

    Testing alternate filebrowser settings for S3 compat

--- curriculumBuilder/s3utils.py
+++ curriculumBuilder/s3utils.py
@@ -1,5 +1,62 @@
-from storages.backends.s3boto import S3BotoStorage
+from storages.backends.s3boto import S3BotoStorage, S3BotoStorageFile
 
-StaticRootS3BotoStorage = lambda: S3BotoStorage(location='static')
-MediaRootS3BotoStorage  = lambda: S3BotoStorage(location='media')
-CurriculumRootS3BotoStorage  = lambda: S3BotoStorage(location='curriculum')
+# StaticRootS3BotoStorage = lambda: S3BotoStorage(location='static')
+# MediaRootS3BotoStorage  = lambda: S3BotoStorage(location='media')
+# CurriculumRootS3BotoStorage  = lambda: S3BotoStorage(location='curriculum')
+
+
+class S3BotoStorageSafe(S3BotoStorage):
+    def __init__(self, *args, **kwargs):
+        return super(S3BotoStorageSafe, self).__init__(*args, **kwargs)
+
+    def isfile(self, name):
+        try:
+            name = self._normalize_name(self._clean_name(name))
+            f = S3BotoStorageFile(name, 'rb', self)
+            if not f.key:
+                return False
+            return True
+        except:
+            return False
+
+    def isdir(self, name):
+        return not self.isfile(name)
+
+    def move(self, old_file_name, new_file_name, allow_overwrite=False):
+
+        if self.exists(new_file_name):
+            if allow_overwrite:
+                self.delete(new_file_name)
+            else:
+                raise "The destination file '%s' exists and allow_overwrite is False" % new_file_name
+
+        old_key_name = self._encode_name(self._normalize_name(self._clean_name(old_file_name)))
+        new_key_name = self._encode_name(self._normalize_name(self._clean_name(new_file_name)))
+
+        k = self.bucket.copy_key(new_key_name, self.bucket.name, old_key_name)
+
+        if not k:
+            raise "Couldn't copy '%s' to '%s'" % (old_file_name, new_file_name)
+
+        self.delete(old_file_name)
+
+    def makedirs(self, name):
+        # i can't create dirs still
+        pass
+
+    def rmtree(self, name):
+        name = self._normalize_name(self._clean_name(name))
+        dirlist = self.bucket.list(self._encode_name(name))
+        for item in dirlist:
+            item.delete()
+
+    def save(self, name, content):
+        re = super(S3BotoStorageSafe, self).save(name, content)
+        # key.copy(key.bucket, key.name, preserve_acl=True, metadata={'Content-Type': 'text/plain'})
+        return re
+
+
+StaticRootS3BotoStorage = lambda: S3BotoStorageSafe(location='static')
+StaticStagingS3BotoStorage = lambda: S3BotoStorageSafe(location='static_staging')
+MediaRootS3BotoStorage = lambda: S3BotoStorageSafe(location='media')
+CurriculumRootS3BotoStorage = lambda: S3BotoStorageSafe(location='curriculum')
--- curriculumBuilder/settings.py
+++ curriculumBuilder/settings.py
@@ -389,7 +389,7 @@ OPTIONAL_APPS = (
     "debug_toolbar",
     "django_extensions",
     "compressor",
-    # PACKAGE_NAME_FILEBROWSER,
+    PACKAGE_NAME_FILEBROWSER,
     PACKAGE_NAME_GRAPPELLI,
 )
 
@@ -505,9 +505,15 @@ if ON_PAAS:
 
     AWS_BASE_URL = 'http://cdo-curriculum.s3-website-us-east-1.amazonaws.com'
 
-    STATICFILES_LOCATION = 'static'
-    STATICFILES_STORAGE = 'curriculumBuilder.s3utils.StaticRootS3BotoStorage'
-    STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+    if os.environ.get('ENV') == 'staging':
+        STATICFILES_LOCATION = 'static_staging'
+        STATICFILES_STORAGE = 'curriculumBuilder.s3utils.StaticStagingS3BotoStorage'
+        STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+
+    else:
+        STATICFILES_LOCATION = 'static'
+        STATICFILES_STORAGE = 'curriculumBuilder.s3utils.StaticRootS3BotoStorage'
+        STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
 
     MEDIAFILES_LOCATION = 'media'
     DEFAULT_FILE_STORAGE = 'curriculumBuilder.s3utils.MediaRootS3BotoStorage'
