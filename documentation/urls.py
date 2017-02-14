c96f9e494d186df9c6745ac222a02ec13bc882c6
Josh Caldwell <josh@code.org>
2017-02-14 11:12:03 -0800

    Move curric docs under /curriculum and fix editability for optional lessons on unit at a glance page.

--- documentation/urls.py
+++ documentation/urls.py
@@ -4,8 +4,8 @@ from documentation import views
 
 urlpatterns = patterns('curricula.views',
                         #url(r'^$', views.index, name='home'),
-                        url(r'^(?P<curric_slug>[-\w]+)/(?P<slug>[-\w.]+)/$', views.page_view, name='page_view'),
                         url(r'^(?P<slug>[-\w]+)/$', views.ide_view, name='ide_view'),
                         url(r'^(?P<ide_slug>[-\w]+)/(?P<slug>[-\w.]+)/$', views.block_view, name='block_view'),
                         url(r'^(?P<ide_slug>[-\w]+)/(?P<slug>[-\w.]+)/embed/$', views.embed_view, name='embed_view'),
+                        url(r'^curriculum/(?P<curric_slug>[-\w]+)/(?P<slug>[-\w.]+)/$', views.page_view, name='page_view'),
                        )
