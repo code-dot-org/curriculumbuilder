6fac7518777938d845236ed380e15bf110470b29
Josh Caldwell <jpcaldwell@gmail.com>
2017-09-29 14:15:19 -0700

    Make waitress streaming request friendly
    Without setting send_bytes to a small value, waitress waits way too long to start streaming requests, causing timeouts

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from curriculumBuilder import wsgi
 # touch app-initialized
 open('/tmp/app-initialized', 'w').close()
 # start waitress
-serve(wsgi.application, unix_socket='/tmp/nginx.socket')
+serve(wsgi.application, unix_socket='/tmp/nginx.socket', send_bytes=1)
