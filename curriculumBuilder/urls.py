f1c169ead0e56d5ccdec2e6aeef23c1fe6e9032e
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-05-14 15:18:32 -0700

    dont i18n admin url patterns

--- curriculumBuilder/urls.py
+++ curriculumBuilder/urls.py
@@ -27,7 +27,7 @@ admin.autodiscover()
 # You can also change the ``home`` view to add your own functionality
 # to the project's homepage.
 
-urlpatterns = i18n_patterns("",
+urlpatterns = patterns("",
     # Change the admin prefix here to use an alternate URL for the
     # admin interface, which would be marginally more secure.
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
