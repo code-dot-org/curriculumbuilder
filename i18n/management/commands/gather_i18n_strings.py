33a2924912393d331580a506a2617c165f68215d
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-06-06 09:21:11 -0700

    update i18n sync processes to use I18nFileWrapper

--- i18n/management/commands/gather_i18n_strings.py
+++ i18n/management/commands/gather_i18n_strings.py
@@ -1,16 +1,12 @@
 from i18n.models import Internationalizable
+from i18n.utils import I18nFileWrapper
 from django.core.management.base import BaseCommand, CommandError
 
 import django.apps
-import json
-import os
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        staticfiles = os.path.join(os.path.dirname(__file__), '../../static/source')
-        if not os.path.exists(staticfiles):
-            os.makedirs(staticfiles)
-
         for model in django.apps.apps.get_models():
             # We care about models that:
             #   extend the InternationalizablePage models defined by this module
@@ -19,6 +15,4 @@ class Command(BaseCommand):
             is_not_proxy = not model._meta.proxy
             if (is_internationalizable and is_not_proxy):
                 strings = model.gather_strings()
-                outpath = os.path.abspath(os.path.join(staticfiles, model.__name__ + ".json"))
-                with open(outpath, 'w') as outfile:
-                    json.dump(strings, outfile, indent=4, sort_keys=True)
+                I18nFileWrapper.write_source(model.__name__, strings)
--- i18n/models.py
+++ i18n/models.py
@@ -1,16 +1,15 @@
 from __future__ import print_function
 
-import json
-import os
 import sys
 import time
 
 from django.conf import settings
-from django.core.cache import cache
 from django.db import models
 from django.utils import translation
 
 from mezzanine.pages.models import Page
+
+from .utils import I18nFileWrapper
 
 
 class Internationalizable(models.Model):
@@ -86,46 +85,11 @@ class Internationalizable(models.Model):
             return
 
         for field in self.__class__.internationalizable_fields():
-            translated = self.get_translated_field(field, lang)
+            translated = I18nFileWrapper.get_translated_field(self.__class__.__name__, self.i18n_key, field, lang)
             if translated:
                 if field not in self._untranslated_values:
                     self._untranslated_values[field] = getattr(self, field)
                 setattr(self, field, translated)
-
-    def load_translations(self, lang):
-        translation_file = os.path.join(os.path.dirname(__file__), 'static', 'translations', lang, self.__class__.__name__ + '.json')
-        cached = cache.get(translation_file)
-        if cached is None:
-            try:
-                with open(translation_file, 'r') as translation_json:
-                    try:
-                        translations = json.load(translation_json)
-                    except ValueError:
-                        # Could not parse the translation file; this should not
-                        # happen, and probably represents a significant problem
-                        # in the i18n sync process
-                        translations = {}
-                    cached = translations
-            except IOError:
-                # Could not find the translation file; most likely the locale
-                # string is malformed or simply referencing a language we are
-                # not yet translating
-                cached = {}
-            cache.set(translation_file, cached)
-
-        return cached
-
-    def get_translated_field(self, field, lang):
-        translations = self.load_translations(lang)
-        try:
-            return translations[self.i18n_key][field]
-        except KeyError:
-            # Could not find the specified string in the translation file,
-            # possibly just because the string is new and has not yet been
-            # through the sync process. If crowdin is set up to only export
-            # translated strings, this might also happen whenever a string
-            # exists but has not yet been translated
-            return ""
 
 class InternationalizablePage(Page, Internationalizable):
 
