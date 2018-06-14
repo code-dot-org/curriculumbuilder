4c6030f146e6531dca7c56cc56784bca22986f2a
Elijah Hamovitz <elijahhamovitz@gmail.com>
2018-06-14 16:11:33 -0700

    add publish operation to sync operation

--- bin/sync-i18n.sh
+++ bin/sync-i18n.sh
@@ -5,4 +5,4 @@ set -e
 python manage.py gather_i18n_strings
 python manage.py i18n_sync_up
 python manage.py i18n_sync_down
-echo "i18n sync finished, content is ready to publish"
+python manage.py publish_i18n
