7fe7d2ff1cd8bbacf8c144da68669550134c7620
Josh Caldwell <josh@code.org>
2018-06-19 11:26:10 -0400

    Fix param stripping logic

--- curricula/templatetags/level_link.py
+++ curricula/templatetags/level_link.py
@@ -5,5 +5,8 @@ register = template.Library()
 
 @register.filter(name='level_link')
 def level_link(link):
-
-    return "https://studio.code.org%s" % link[:link.find('?')]
+    param_index = link.find('?')
+    if param_index > -1:
+        return "https://studio.code.org%s" % link[:param_index]
+    else:
+        return "https://studio.code.org%s" % link
