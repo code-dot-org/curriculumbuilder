c21b40654fe43ba9f830d327a1596aaa473b4534
Josh Caldwell <josh@code.org>
2018-09-12 10:54:06 -0700

    CSV library can handle booleans, swapping out strings

--- standards/views.py
+++ standards/views.py
@@ -51,10 +51,10 @@ def by_curriculum_csv(request, slug):
         for lesson in unit.lessons:
             for standard in lesson.standards.all():
                 writer.writerow([curriculum.slug, unit.slug, 'lesson %d' % lesson.number, lesson.title,
-                                 standard.framework.slug, standard.shortcode, 'false'])
+                                 standard.framework.slug, standard.shortcode, False])
             for standard in lesson.opportunity_standards.all():
                 writer.writerow([curriculum.slug, unit.slug, 'lesson %d' % lesson.number, lesson.title,
-                                 standard.framework.slug, standard.shortcode, 'true'])
+                                 standard.framework.slug, standard.shortcode, True])
     return response
 
 
