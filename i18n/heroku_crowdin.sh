d82a518f8957882ad282fd210288ecb2c7a810da
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-01-17 17:00:30 -0800

    fix automatic i18n sync

--- i18n/heroku_crowdin.sh
+++ i18n/heroku_crowdin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script exists solely to run heroku on crowdin. It's necessary for two
+# This script exists solely to run heroku on crowdin. It's necessary for three
 # reasons:
 #
 # First, the heroku apt buildpack doesn't correctly add the installed crowdin
@@ -10,9 +10,14 @@
 # path. This might be because the default-jre package expects
 # update-alternatives to set up the symlinks, but the actual cause is unclear.
 #
+# Third, the heroku scheduler sets $JAVA_TOOL_OPTIONS to some value that causes
+# this manual invocation of java to break (an environment state that is not
+# replicated when running scripts with `heroku run`), so we need to make sure
+# it's unset for this invocation.
+#
 # Either way, this script exists solely to get around that, and if we are ever
 # able to come up with a clean way to install the crowdin-cli and its
 # dependencies on heroku this can be removed.
 
 apt_dir="${BASH_SOURCE%/*}/../.apt/"
-$apt_dir/usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar "$apt_dir/usr/local/bin/crowdin-cli.jar" "$@"
+JAVA_TOOL_OPTIONS= $apt_dir/usr/lib/jvm/java-8-openjdk-amd64/bin/java -jar "$apt_dir/usr/local/bin/crowdin-cli.jar" "$@"
