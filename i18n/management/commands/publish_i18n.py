5bd006f716a6598444cc9a8143db1898a25debc8
Elijah Hamovitz <elijahhamovitz@gmail.com>
2019-03-21 12:44:34 -0700

    update the 'publish' stage of the i18n sync to silent the standard publishing log messages, and provide some of its own

--- i18n/management/commands/publish_i18n.py
+++ i18n/management/commands/publish_i18n.py
@@ -9,26 +9,34 @@ from django.utils import translation
 from i18n.models import Internationalizable
 from i18n.utils import print_clear
 
+from django_slack import slack_message
+
+def should_publish_model(model):
+    is_internationalizable = issubclass(model, Internationalizable)
+    is_not_proxy = not model._meta.proxy
+    has_publish_operation = hasattr(model, 'publish') or hasattr(model, 'publish_pdfs')
+    return is_internationalizable and is_not_proxy and has_publish_operation
+
+def log(message):
+    print(message)
+    slack_message('slack/message.slack', {
+        'message': message
+    })
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        print("Publishing translated content")
-        for model in django.apps.apps.get_models():
-            is_internationalizable = issubclass(model, Internationalizable)
-            is_not_proxy = not model._meta.proxy
-            if not (is_internationalizable and is_not_proxy):
-                continue
+        log("Publishing translated content")
+        models = [model for model in django.apps.apps.get_models() if should_publish_model(model)]
+        log("Models to publish: %s" % ', '.join(model.__name__ for model in models))
 
+        for model_index, model in enumerate(models):
             name = model.__name__
-
-            if not hasattr(model, 'publish') and not hasattr(model, 'publish_pdfs'):
-                print("%s - skipping (no publish operation available)" % name)
-                continue
-
-            print_clear("%s - loading" % name)
+            log("Publishing %s (%s/%s)" % (name, model_index + 1, len(models)))
             objects = model.get_i18n_objects()
             total = objects.count()
+            success_count = 0
+
             for index, obj in enumerate(objects.all()):
-                print_clear("%s - publishing %s/%s" % (name, index, total))
                 if not obj.should_be_translated:
                     continue
 
@@ -39,9 +47,10 @@ class Command(BaseCommand):
 
                     translation.activate(language_code)
                     if hasattr(obj, 'publish'):
-                        list(obj.publish())
+                        list(obj.publish(silent=True))
                     if hasattr(obj, 'publish_pdfs'):
-                        list(obj.publish_pdfs())
+                        list(obj.publish_pdfs(silent=True))
+                success_count += 1
 
                 translation.activate(original_lang)
-            print_clear("%s - finished" % name, end='\n')
+            log("%s/%s %s objects published" % (success_count, total, name))
