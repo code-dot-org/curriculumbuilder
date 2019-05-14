618864c920b8589e83a58548ca5307e186a6a8f3
Erin Bond <erin.bond@code.org>
2019-05-14 10:49:21 -0700

    append random number as a separate param

--- curricula/templatetags/tracking_pixel_url.py
+++ curricula/templatetags/tracking_pixel_url.py
@@ -5,10 +5,11 @@ import random
 register = template.Library()
 
 @register.filter(name='tracking_pixel_url')
+
 def tracking_pixel_url(path):
     # Append a random number so that the tracking_pixel
     # isn't cached - we want each visit to a url to go
     # fetch a new tracking pixel image.
     random_num = random.randint(1,1001)
-    tracking_pixel_url = 'https://studio.code.org/tracking_pixel?from=' + urllib.quote_plus(path) + str(random_num)
+    tracking_pixel_url = 'http://localhost-studio.code.org:3000/tracking_pixel?from=' + urllib.quote_plus(path) + '&r=' + str(random_num)
     return tracking_pixel_url
