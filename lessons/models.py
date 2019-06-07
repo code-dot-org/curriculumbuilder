a84b73de6314eb8181d2117856b0163cbfc93ec7
Josh Lory <josh.lory@code.org>
2019-06-07 15:13:34 -0700

    Code review feedback
    Co-Authored-By: Elijah Hamovitz <elijahhamovitz@gmail.com>

--- lessons/models.py
+++ lessons/models.py
@@ -72,7 +72,7 @@ class Vocab(Internationalizable):
 
     @property
     def should_be_translated(self):
-        return any(lesson.should_be_translated for lesson in self.lesson_set.all()) and not self.mathy
+        return not self.mathy and any(lesson.should_be_translated for lesson in self.lesson_set.all())
 
     def __unicode__(self):
         if self.mathy:
