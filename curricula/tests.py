3065880e593e0b49e06050daf3f1384136ed79d5
David Bailey <davidsbailey@users.noreply.github.com>
2019-09-13 14:20:41 -0700

    test a few more curriculum routes

--- curricula/tests.py
+++ curricula/tests.py
@@ -2,6 +2,10 @@ from django.test import TestCase
 
 from mezzanine.core.templatetags.mezzanine_tags import richtext_filters
 
+from curricula.models import Curriculum, Unit
+from lessons.models import Lesson
+from standards.models import Category, GradeBand, Framework
+
 class IframeDomainRestrictionTestCase(TestCase):
     """
     Test the RICHTEXT_ALLOWED_ATTRIBUTES logic that will only allow iframes to
@@ -22,6 +26,79 @@ class IframeDomainRestrictionTestCase(TestCase):
                          richtext_filters('<iframe src="docs.google.com"></iframe>'))
 
 class CurriculaRenderingTestCase(TestCase):
+    def setUp(self):
+        self.test_curriculum = Curriculum.objects.create(title="Test Curriculum", slug="test-curriculum")
+        self.test_unit = Unit.objects.create(
+            title="Test Unit",
+            parent=self.test_curriculum,
+            slug="test-unit",
+            description="Test unit description",
+            show_calendar=True
+        )
+        self.hoc_unit = Unit.objects.create(
+            title="HoC Unit",
+            parent=self.test_curriculum,
+            slug="hoc-unit",
+            description="Hoc unit description",
+            lesson_template_override="curricula/hoc_lesson.html"
+        )
+        self.csf_unit = Unit.objects.create(
+            title="CSF Unit",
+            parent=self.test_curriculum,
+            slug="csf-unit",
+            description="CSF unit description",
+            lesson_template_override="curricula/csf_lesson.html"
+        )
+        self.pl_unit = Unit.objects.create(
+            title="PL Unit",
+            parent=self.test_curriculum,
+            slug="pl-unit",
+            description="PL unit description",
+            lesson_template_override="curricula/pl_lesson.html"
+        )
+        self.test_lesson = Lesson.objects.create(title="Test Lesson", parent=self.test_unit)
+        self.hoc_lesson = Lesson.objects.create(title="HoC Lesson", parent=self.hoc_unit)
+        self.csf_lesson = Lesson.objects.create(title="CSF Lesson", parent=self.csf_unit)
+        self.pl_lesson = Lesson.objects.create(title="PL Lesson", parent=self.pl_unit)
+
     def test_homepage(self):
         response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_render_curriculum(self):
+        response = self.client.get('/test-curriculum/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_render_unit(self):
+        response = self.client.get('/test-curriculum/test-unit/')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get('/test-curriculum/csf-unit/')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get('/test-curriculum/hoc-unit/')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get('/test-curriculum/pl-unit/')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get('/test-curriculum/test-unit/glance/')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get('/test-curriculum/test-unit/vocab/')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get('/test-curriculum/test-unit/code/')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get('/test-curriculum/test-unit/resources/')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get('/test-curriculum/test-unit/objectives/')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get('/test-curriculum/test-unit/unit_feedback/')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get('/test-curriculum/test-unit/?pdf=1')
+        self.assertEqual(response.status_code, 200)
+
+    def test_render_lesson(self):
+        response = self.client.get('/test-curriculum/test-unit/1/')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get('/test-curriculum/hoc-unit/1/')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get('/test-curriculum/csf-unit/1/')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get('/test-curriculum/pl-unit/1/')
         self.assertEqual(response.status_code, 200)
