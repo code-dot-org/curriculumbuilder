83158d2f4fb457318f226a4cced7a36967090702
David Bailey <davidsbailey@users.noreply.github.com>
2019-10-30 14:33:07 -0700

    add unit test for hiding privileged resource fields

--- lessons/tests/test_lessons_admin.py
+++ lessons/tests/test_lessons_admin.py
@@ -1,4 +1,8 @@
 from django.test import TestCase
+from django.contrib.auth.models import Permission
+from django.contrib.sites.models import Site
+
+from mezzanine.core.models import SitePermission
 
 from curricula.factories import UserFactory
 
@@ -11,8 +15,27 @@ class LessonsAdminTestCase(TestCase):
         user.is_staff = True
         user.save()
         self.user = user
-        self.client.login(username=user.username, password='password')
+        self.assertTrue(self.client.login(username=user.username, password='password'))
+
+        site = Site.objects.first()
+        siteperms = SitePermission.objects.create(user=user)
+        siteperms.sites.add(site)
 
     def test_render_add_resource(self):
+        permission = Permission.objects.get(codename='add_resource')
+        self.user.user_permissions.add(permission)
+
         response = self.client.get('/admin/lessons/resource/add/')
         self.assertEqual(response.status_code, 200)
+        self.assertIn('Add resource', response.content)
+        self.assertIn('Download URL', response.content)
+        self.assertNotIn('Force I18n', response.content, 'privileged field should not be visible')
+
+        permission = Permission.objects.get(codename='access_all_resources')
+        self.user.user_permissions.add(permission)
+
+        response = self.client.get('/admin/lessons/resource/add/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Add resource', response.content)
+        self.assertIn('Download URL', response.content)
+        self.assertIn('Force I18n', response.content, 'privileged field should be visible')
