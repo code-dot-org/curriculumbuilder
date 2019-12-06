2ff4b70aa8f76aece39bb5b7550ea907d3a2bdd9
Dani McAvoy <danimcavoy@gmail.com>
2019-12-06 10:58:47 -0500

    Improve unit tests

--- documentation/tests/test_map_model.py
+++ documentation/tests/test_map_model.py
@@ -6,11 +6,12 @@ from documentation.factories import MapFactory
 class MapModelTestCase(TestCase):
 
     def setUp(self):
-        self.myMap = MapFactory(title='my concept', slug='concepts/myConcept')
-        self.myChildMap = MapFactory(title='my concept', slug='concepts/myConcept', parent=self.myMap)
+        self.myMap = MapFactory(title='my concept parent', slug='concepts/myConcept')
+        self.myChildMap = MapFactory(title='my concept child', slug='concepts/myConcept/childConcept', parent=self.myMap)
 
     def test_get_absolute_url(self):
         self.assertEqual('/docs/concepts/myConcept/', self.myMap.get_absolute_url())
+        self.assertEqual('/docs/concepts/myConcept/childConcept/', self.myChildMap.get_absolute_url())
 
     def test_get_children(self):
         self.assertIn(self.myChildMap, self.myMap.get_children())
