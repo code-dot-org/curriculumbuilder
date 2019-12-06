6d122457db22ae8e50b8b2b22d03f9c9f93fbd59
Dani McAvoy <danimcavoy@gmail.com>
2019-12-06 16:39:39 -0500

    remove old tests

--- documentation/tests/test_map_model.py
+++ documentation/tests/test_map_model.py
@@ -9,13 +9,5 @@ class MapModelTestCase(TestCase):
         self.myMap = MapFactory(title='my concept parent', slug='concepts/myConcept')
         self.myChildMap = MapFactory(title='my concept child', slug='concepts/myConcept/childConcept', parent=self.myMap)
 
-    def test_get_absolute_url_for_curriculum_code_org_map_menu(self):
-        self.assertEqual('/documentation/concepts/myConcept/', self.myMap.get_absolute_url_for_curriculum_code_org_map_menu())
-        self.assertEqual('/documentation/concepts/myConcept/childConcept/', self.myChildMap.get_absolute_url_for_curriculum_code_org_map_menu())
-
-    def test_get_absolute_url_for_studio_code_org_map_menu(self):
-        self.assertEqual('/docs/concepts/myConcept/', self.myMap.get_absolute_url_for_studio_code_org_map_menu())
-        self.assertEqual('/docs/concepts/myConcept/childConcept/', self.myChildMap.get_absolute_url_for_studio_code_org_map_menu())
-
     def test_get_children(self):
         self.assertIn(self.myChildMap, self.myMap.get_children())
