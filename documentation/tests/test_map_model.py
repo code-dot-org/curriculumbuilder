e2916b7079752ff05a449add76adfede279d6ce9
Dani McAvoy <danimcavoy@gmail.com>
2019-12-09 11:16:25 -0500

    Add unit test for get_absolute_url_for_host filter

--- documentation/tests/test_map_model.py
+++ documentation/tests/test_map_model.py
@@ -2,7 +2,8 @@ from django.test import TestCase
 
 from documentation.factories import MapFactory
 
-# Tests for admin pages in lessons/admin.py
+from documentation.templatetags.documentation_extras import get_absolute_url_for_host
+
 class MapModelTestCase(TestCase):
 
     def setUp(self):
@@ -11,3 +12,9 @@ class MapModelTestCase(TestCase):
 
     def test_get_children(self):
         self.assertIn(self.myChildMap, self.myMap.get_children())
+
+    def test_get_absolute_url_for_host(self):
+        result = get_absolute_url_for_host(self.myMap, 'curriculum.code.org')
+        self.assertEqual(result, '/documentation/concepts/myConcept/')
+        result2 = get_absolute_url_for_host(self.myMap, 'studio.code.org')
+        self.assertEqual(result2, '/docs/concepts/myConcept/')
