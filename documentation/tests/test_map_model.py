741e765c63b8530cd5d2b71f6df8f10106e1eaff
Dani LaMarca <dani@code.org>
2019-12-11 09:42:54 -0500

    Revert "Give a different url depending on the host"

--- documentation/tests/test_map_model.py
+++ documentation/tests/test_map_model.py
@@ -2,19 +2,16 @@ from django.test import TestCase
 
 from documentation.factories import MapFactory
 
-from documentation.templatetags.documentation_extras import get_absolute_url_for_host
-
+# Tests for admin pages in lessons/admin.py
 class MapModelTestCase(TestCase):
 
     def setUp(self):
         self.myMap = MapFactory(title='my concept parent', slug='concepts/myConcept')
         self.myChildMap = MapFactory(title='my concept child', slug='concepts/myConcept/childConcept', parent=self.myMap)
 
+    def test_get_absolute_url(self):
+        self.assertEqual('/docs/concepts/myConcept/', self.myMap.get_absolute_url())
+        self.assertEqual('/docs/concepts/myConcept/childConcept/', self.myChildMap.get_absolute_url())
+
     def test_get_children(self):
         self.assertIn(self.myChildMap, self.myMap.get_children())
-
-    def test_get_absolute_url_for_host(self):
-        result = get_absolute_url_for_host(self.myMap, 'curriculum.code.org')
-        self.assertEqual(result, '/documentation/concepts/myConcept/')
-        result2 = get_absolute_url_for_host(self.myMap, 'studio.code.org')
-        self.assertEqual(result2, '/docs/concepts/myConcept/')
