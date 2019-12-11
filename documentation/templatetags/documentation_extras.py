1a98b00f720601a3e3bd8349f2d8033bed00e2aa
Dani McAvoy <danimcavoy@gmail.com>
2019-12-11 09:08:06 -0500

    Keep curriculum.code.org

--- documentation/templatetags/documentation_extras.py
+++ documentation/templatetags/documentation_extras.py
@@ -5,7 +5,7 @@ register = template.Library()
 
 @register.filter
 def get_absolute_url_for_host(map, host):
-    if host == 'codecurricula.com' or host == 'localhost:8000':
+    if host == 'curriculum.code.org' or host == 'codecurricula.com' or host == 'localhost:8000':
         return '/documentation/%s/' % map.slug
     elif host == 'studio.code.org':
         return '/docs/%s/' % map.slug
