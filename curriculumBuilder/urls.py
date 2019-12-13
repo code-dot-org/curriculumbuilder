9dca44a61d3d04f1750e70a55111c4b183925cd6
Dani McAvoy <danimcavoy@gmail.com>
2019-12-13 13:40:33 -0500

    Remove redirect and just keep /documentation link

--- curriculumBuilder/urls.py
+++ curriculumBuilder/urls.py
@@ -23,8 +23,6 @@ from gong import views as gong_views
 
 from i18n.urls import i18n_patterns_no_default_language_prefix
 
-from .views import redirect_docs
-
 admin.autodiscover()
 
 # Add the urlpatterns for any custom Django applications here.
@@ -110,7 +108,7 @@ urlpatterns += patterns('',
     url(r'^admin_keywords_submit/$', generic_views.admin_keywords_submit, name='admin_keywords_submit'),
     url("^None/$", views.index),  # Dealing with JackFrost bug
     url(r'^docs/', include('documentation.urls', namespace="documentation")),
-    url(r'^documentation/', redirect_docs),
+    url(r'^documentation/', include('documentation.urls', namespace="documentation")),
     url(r'^standards/', include('standards.urls', namespace="standards")),
 )
 
--- curriculumBuilder/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import redirect
-
-def redirect_docs(request):
-    response = redirect('/docs/' + request.path.split('/documentation/')[1])
-    return response
