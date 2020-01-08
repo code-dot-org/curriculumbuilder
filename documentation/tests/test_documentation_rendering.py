8de1a53069ba1aa3a708647decbe3d105a557c58
Dani McAvoy <danimcavoy@gmail.com>
2020-01-08 09:35:49 -0500

    Create test for ide docs pages

--- documentation/tests/test_documentation_rendering.py
+++ documentation/tests/test_documentation_rendering.py
@@ -3,13 +3,14 @@ from django.test import TestCase
 from curricula.factories import CurriculumFactory, UnitFactory
 from lessons.factories import LessonFactory
 
-from documentation.factories import IDEFactory, BlockFactory
+from documentation.factories import IDEFactory, BlockFactory, CategoryFactory
 
 
 class DocumentationRenderingTestCase(TestCase):
     def setUp(self):
-        self.myIDE = IDEFactory(slug="myIDE")
-        self.myBlock = BlockFactory(slug="myBlock", title="My Block", parent_ide=self.myIDE, parent=self.myIDE)
+        self.myIDE = IDEFactory(slug="mylab")
+        self.myCategory = CategoryFactory(name="myCategory", parent_ide=self.myIDE)
+        self.myBlock = BlockFactory(slug="myBlock", title="myBlock", parent_ide=self.myIDE, parent=self.myIDE, parent_cat=self.myCategory)
 
         self.test_curriculum = CurriculumFactory(slug="test-curriculum")
         self.test_unit = UnitFactory(parent=self.test_curriculum, slug="test-unit")
@@ -21,10 +22,21 @@ class DocumentationRenderingTestCase(TestCase):
         response = self.client.get('/test-curriculum/code/')
         self.assertEqual(response.status_code, 200)
         self.assertIn('My Lesson', response.content)
-        self.assertIn('My Block', response.content)
+        self.assertIn('myBlock', response.content)
 
     def test_render_lesson_includes_blocks(self):
         response = self.client.get('/test-curriculum/test-unit/1/')
         self.assertEqual(response.status_code, 200)
         self.assertIn('Introduced Code', response.content)
-        self.assertIn('My Block', response.content)
+        self.assertIn('myBlock', response.content)
+
+    def test_render_ide_blocks(self):
+        response = self.client.get('/documentation/mylab/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Try it out', response.content)
+        self.assertIn('myCategory', response.content)
+        self.assertIn('myBlock', response.content)
+        response2 = self.client.get('/documentation/mylab/myBlock/')
+        self.assertEqual(response2.status_code, 200)
+        self.assertIn('myCategory', response2.content)
+        self.assertIn('myBlock', response2.content)
