8525f33cc1b99bf316761df9ef1a319bc43089f2
Dani McAvoy <danimcavoy@gmail.com>
2020-01-08 11:30:02 -0500

    Add tests to check admin add pages for standards and documentation

---
+++ standards/tests/test_standards_admin.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+from django.contrib.auth.models import Permission
+from django.contrib.sites.models import Site
+
+from mezzanine.core.models import SitePermission
+
+from curricula.factories import UserFactory
+
+
+class StandardsAdminTestCase(TestCase):
+    def setUp(self):
+        user = UserFactory()
+        user.is_staff = True
+        user.save()
+        self.user = user
+        self.assertTrue(self.client.login(username=user.username, password='password'))
+
+        site = Site.objects.first()
+        siteperms = SitePermission.objects.create(user=user)
+        siteperms.sites.add(site)
+
+    def render_add(self, type):
+        response = self.client.get('/admin/standards/' + type + '/add/')
+        self.assertEqual(response.status_code, 403)
+
+        permission = Permission.objects.get(content_type__app_label='standards', codename='add_' + type)
+        self.user.user_permissions.add(permission)
+
+        response = self.client.get('/admin/standards/' + type + '/add/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Add ' + type, response.content)
+
+    def test_render_add_category(self):
+        self.render_add('category')
+
+    def test_render_add_standard(self):
+        self.render_add('standard')
+
+    def test_render_add_framework(self):
+        self.render_add('framework')
