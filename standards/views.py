6807f6fb259ca7e8487ca97c48165a9f25a2adac
Clare Constantine <clare@code.org>
2020-02-24 16:30:23 -0800

    import reduce function

--- standards/views.py
+++ standards/views.py
@@ -12,6 +12,7 @@ from rest_framework.response import Response
 from curricula.models import Curriculum, Unit
 from standards.models import *
 from standards.serializers import *
+from functools import reduce
 
 
 def index(request):
