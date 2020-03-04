de51e342b0dca984d4a27b8a0dadbf558c946e04
reborn <zakaden@gmail.com>
2020-03-03 17:44:33 -0800

    Clean up pdfUtils
    change to using subprocess.call from subprocess.Popen,
    use python 'with' clause

--- helpers/pdfUtils.py
+++ helpers/pdfUtils.py
@@ -10,11 +10,10 @@ SCRIPT_PATH = settings.BASE_DIR + "/bin/generate-pdf.js"
 
 def get_pdf_for_url(url):
   try:
-    tmp = tempfile.NamedTemporaryFile(delete=True)
-    child = subprocess.Popen(["node", SCRIPT_PATH, "-w", tmp.name, "-u %s" % (url)], stdout=subprocess.PIPE)
-    child.communicate()
-    contents = tmp.read()
-    tmp.close()
-    return contents
+    with tempfile.NamedTemporaryFile(delete=True) as tmp:
+      child = subprocess.call(["node", SCRIPT_PATH, "-w", tmp.name, "-u %s" % (url)])
+      contents = tmp.read()
+      tmp.close()
+      return contents
   except CalledProcessError, e:
     logger.exception("getting pdf failed for url %s: %s" % (url, e.output))
