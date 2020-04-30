9896d6068ab574cd220a0d3d52647b850f512b09
Bethany Connor <bethany@code.org>
2020-04-30 23:53:48 +0000

    Mock out getting translations

--- i18n/tests/test_url_resolver.py
+++ i18n/tests/test_url_resolver.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
 from django.contrib.auth.models import Permission
 from django.core.cache import cache
 from django.utils import translation
@@ -6,16 +6,16 @@ from django.utils import translation
 from curricula.factories import UserFactory, CurriculumFactory, UnitFactory
 from lessons.factories import LessonFactory, ResourceFactory, ActivityFactory
 from django.conf import settings
+from i18n.utils import I18nFileWrapper
+from mock import patch, Mock
 
+@patch('i18n.utils.I18nFileWrapper.get_translated_field', Mock())
 class I18nUrlResolverTestCase(TestCase):
     def setUp(self):
         self.csf_curriculum = CurriculumFactory(
             slug="csf-1718",
             unit_template_override='curricula/csf_unit.html')
-        self.csf_unit = UnitFactory(
-            parent=self.csf_curriculum,
-            slug="coursea",
-            lesson_template_override="curricula/csf_lesson.html")
+        
         # URLs that start with a language prefix (in this case Polish)
         # don't work by default in Django. Check that they do in our system.
         self.pl_curriculum = CurriculumFactory(
@@ -27,10 +27,16 @@ class I18nUrlResolverTestCase(TestCase):
             lesson_template_override="curricula/pl_lesson.html")
 
     def test_render_curriculum(self):
-        with self.settings(I18N_STORAGE='django.core.files.storage.FileSystemStorage'):
             response = self.client.get('/csf-1718/')
             self.assertEqual(response.status_code, 200)
             for language_code, _ in settings.LANGUAGES:
+                cache.set("translations/%s/LC_MESSAGES/Curriculum.json" % translation.to_locale(language_code), {
+            "1": {
+                "description": u"Translated Description",
+                "title": u"Translated Title"
+            }
+        })
+
                 response = self.client.get('/%s/csf-1718/' % language_code)
                 self.assertEqual(response.status_code, 200, "failed for language %s" % language_code)
                 self.assertEqual(language_code, translation.get_language())
