644c26ba20eed2e7e93025b8e2c6ebd53a0e6485
Bethany Connor <bethany@code.org>
2020-05-01 15:54:32 +0000

    Fix test_url_resolver

--- i18n/tests/test_url_resolver.py
+++ i18n/tests/test_url_resolver.py
@@ -5,16 +5,16 @@ from django.utils import translation
 from curricula.factories import UserFactory, CurriculumFactory, UnitFactory
 from lessons.factories import LessonFactory, ResourceFactory, ActivityFactory
 from django.conf import settings
+from mock import patch, Mock
 
+
+@patch('i18n.utils.I18nFileWrapper.get_translated_field', Mock())
 class I18nUrlResolverTestCase(TestCase):
     def setUp(self):
         self.csf_curriculum = CurriculumFactory(
             slug="csf-curriculum",
             unit_template_override='curricula/csf_unit.html')
-        self.csf_unit = UnitFactory(
-            parent=self.csf_curriculum,
-            slug="csf-unit",
-            lesson_template_override="curricula/csf_lesson.html")
+
         # URLs that start with a language prefix (in this case Polish)
         # don't work by default in Django. Check that they do in our system.
         self.pl_curriculum = CurriculumFactory(
@@ -32,7 +32,11 @@ class I18nUrlResolverTestCase(TestCase):
             response = self.client.get('/%s/csf-curriculum/' % language_code)
             self.assertEqual(response.status_code, 200, "failed for language %s" % language_code)
             self.assertEqual(language_code, translation.get_language())
+        # Unfortunately, other tests assume that the language activated will be English
+        # so let's clean up after ourselves.
+        translation.activate(settings.LANGUAGE_CODE)
 
     def test_pl_lesson(self):
         response = self.client.get('/pl-curriculum/pl-unit/')
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(settings.LANGUAGE_CODE, translation.get_language())
