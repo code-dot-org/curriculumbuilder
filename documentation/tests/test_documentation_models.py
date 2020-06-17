54a4466979d5837d939da0be80bb0b7fceae708b
Max <zakaden@gmail.com>
2020-06-17 01:00:59 -0700

    add model unit tests for example

--- documentation/tests/test_documentation_models.py
+++ documentation/tests/test_documentation_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 from documentation.factories import MapFactory, IDEFactory, BlockFactory
+from documentation.models import Example
 
 class DocumentationModelsTestCase(TestCase):
 
@@ -26,3 +27,16 @@ class DocumentationModelsTestCase(TestCase):
         self.assertEqual(result1, ['/documentation/applab/', '/docs/applab/'])
         result2 = self.myBlock.jackfrost_urls()
         self.assertEqual(result2, ['/documentation/applab/onEvent/', '/documentation/applab/onEvent/embed/', '/docs/applab/onEvent/', '/docs/applab/onEvent/embed/'])
+
+    def test_example_embed_app_with_code(self):
+        example = Example(app_display_type=Example.EMBED_APP_WITH_CODE, app='www.testapps.org/appwith-specialcharacter')
+        assert example.get_embed_app_and_code() == 'www.testapps.org/appwith-specialcharacter/embed_app_and_code'
+        assert example.embed_app_with_code_height == 310
+
+    def test_example_embed_app(self):
+        example = Example(app = "http://studio.code.org/projects/spritelab/REKsWyJXYOCJtYWhcBxErg")
+        assert example.get_embed_app() == "https://studio.code.org/projects/spritelab/REKsWyJXYOCJtYWhcBxErg/embed"
+
+    def test_example_default_app_display_type(self):
+        example = Example()
+        assert example.app_display_type ==  Example.CODE_FROM_CODE_FIELD
