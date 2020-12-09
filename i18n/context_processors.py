a55df97eb3f5744cb6254403cb29ed54819c3bd1
Elijah Hamovitz <elijahhamovitz@gmail.com>
2020-12-09 15:09:55 -0800

    add a super basic language selector

--- i18n/context_processors.py
+++ i18n/context_processors.py
@@ -1,4 +1,16 @@
 from django.conf import settings
 
+
 def language_code_do_translation(request):
     return {'LANGUAGE_CODE_DO_TRANSLATION': settings.LANGUAGE_CODE_DO_TRANSLATION}
+
+
+def current_path_without_language(request):
+    current_path = request.get_full_path()
+    current_language = request.LANGUAGE_CODE
+    current_language_prefix = "/" + current_language
+
+    if current_path.startswith(current_language_prefix):
+        return {'CURRENT_PATH_WITHOUT_LANGUAGE': current_path[len(current_language_prefix):]}
+    else:
+        return {'CURRENT_PATH_WITHOUT_LANGUAGE': current_path}
