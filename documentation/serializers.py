107fc813496149974bff56cc7017877b942a749c
Tim Plummer <tim@code.org>
2022-01-19 14:26:08 -0800

    updated schema

--- documentation/serializers.py
+++ documentation/serializers.py
@@ -3,15 +3,11 @@ from rest_framework import serializers
 from documentation.models import Map
 
 class MapExportSerializer(serializers.ModelSerializer):
-    category = serializers.SerializerMethodField()
     children = serializers.SerializerMethodField()
 
     class Meta:
         model = Map
-        fields = ('title', 'content', 'slug', 'category', 'children')
-
-    def get_category(self, obj):
-        return obj.get_absolute_url()
+        fields = ('title', 'content', 'slug', 'children')
 
     def get_children(self, obj):
-        return obj.get_children().map(lambda map: map.name)
+        return map(lambda x: x.slug, obj.get_children())
